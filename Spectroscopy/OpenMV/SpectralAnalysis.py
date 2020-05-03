a42aa17da171721fe1960fed34126194971bc744
Eduardo <eduardo.bemelmans@student.pxl.be>
2020-05-04 01:33:21 +0200

    Update spectral analysis
    # Histogram van spectrum: done
    # JSON file naar PC: todo
    # Meerdere spectra: todo
    # Fixed opstelling van de lichtbron: G4 halogen lamp on order

--- Spectroscopy/OpenMV/SpectralAnalysis.py
+++ Spectroscopy/OpenMV/SpectralAnalysis.py
@@ -1,11 +1,13 @@
 # Spectral analysis - By: Eduardo L. Bemelmans - Tue Apr 28 2020
 
-import sensor, image, time, pyb
+import sensor, image, time, pyb, json
 
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)
 sensor.skip_frames(time = 2000)
+sensor.set_auto_gain(False)
+sensor.set_auto_whitebal(False)
 
 class spectroscopy:
     def __init__(self, boundary_x0, boundary_y0, boundary_x1, boundary_y1,
@@ -23,17 +25,23 @@ class spectroscopy:
 
 clock = time.clock()
 
-spectrum = spectroscopy(100, 135, 220, 135, 220, 147, 135, 147)
+spectrum = spectroscopy(90, 145, 215, 145, 215, 170, 90, 170)
 
 while(True):
     clock.tick()
     img = sensor.snapshot()
-    print(clock.fps())
-    img.draw_rectangle(spectrum.x0, spectrum.y0,
+    # print(clock.fps())
+    spctrArea = img.draw_rectangle(spectrum.x0, spectrum.y0,
         spectrum.x1 - spectrum.x0,
         spectrum.y3 - spectrum.y0,
         color = (255, 255, 255), thickness = 1, fill = False)
+    spctrHistogram = img.get_histogram(bins=8, roi=(spectrum.x0, spectrum.y0,
+        spectrum.x1 - spectrum.x0,
+        spectrum.y3 - spectrum.y0))
+    print(spctrHistogram)
 
-# Histogram van spectrum, JSON file naar PC
-# Meerdere spectra
-# Fixed opstelling van de lichtbron
+
+# Histogram van spectrum: done
+# JSON file naar PC: todo
+# Meerdere spectra: todo
+# Fixed opstelling van de lichtbron: G4 halogen lamp on order
